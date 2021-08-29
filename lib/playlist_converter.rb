34198793ca2b3d6b7ab37ee50294d37cbcc534f0
Liam Noroian <liamnoroian@gmail.com>
2021-08-29 16:04:41 -0700

    Initializing empty gem

---
+++ lib/playlist_converter.rb
@@ -0,0 +1,6 @@
+require "playlist_converter/version"
+
+module PlaylistConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/playlist_converter_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PlaylistConverter do
+  it "has a version number" do
+    expect(PlaylistConverter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
