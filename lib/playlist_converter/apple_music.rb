87cb9b176799f1ca9f04d5eda9bd5c710dac47f2
Liam Noroian <liamnoroian@gmail.com>
2021-08-29 21:13:18 -0700

    Successfully retrieve song information from apple music playlist

--- lib/playlist_converter/apple_music.rb
+++ lib/playlist_converter/apple_music.rb
@@ -1,10 +1,10 @@
 require 'jwt'
 require 'faraday'
+require 'faraday_middleware'
 require 'openssl'
 
 module AppleMusic
   API_URI = 'https://api.music.apple.com/v1/'
-
   class Config
     attr_accessor :auth_token
 
@@ -27,22 +27,40 @@ module AppleMusic
 
       JWT.encode(payload, private_key, algorithm, kid: key_id)
     end
-
   end
 
-  class Client
-    attr_accessor :config, :adapter
-
+  class << self
     def initialize
-      @config = Config.new
-      @adapter = Faraday.new(API_URI) do |conn|
-        conn.headers['Authorization'] = "Bearer #{@config.auth_token}"
+      @auth_token = get_auth_token
+    end
+
+    def config
+      @config ||= Config.new
+    end
+
+    def client
+      @client ||= Faraday.new(API_URI) do |conn|
+        conn.headers['Authorization'] = "Bearer #{config.auth_token}"
         conn.adapter Faraday.default_adapter
       end
+    end
+
+    def method_missing(name, *args, &block)
+      if client.respond_to?(name)
+        client.send(name, *args, &block)
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(name, include_private = false)
+      client.respond_to?(name, include_private)
     end
   end
 end
 
-apple_music = AppleMusic::Client.new
-response = apple_music.adapter.get("catalog/us/playlists/pl.ea8a00ee10e94d7a9002583a337cbd3f")
-puts JSON.pretty_generate(JSON.parse(response.body))
+response = AppleMusic.get("catalog/us/playlists/pl.ea8a00ee10e94d7a9002583a337cbd3f")
+tracks = JSON.parse(response.body)['data'][0]['relationships']['tracks']['data']
+tracks.each do |track|
+  puts JSON.pretty_generate track['attributes']['name']
+end
